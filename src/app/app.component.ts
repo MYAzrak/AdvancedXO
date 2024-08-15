2243247212f847d36e3bb41ef3fd3f6c2af53284
MohammadYA <yaserazrak5@gmail.com>
2024-08-15 13:30:15 +0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +12,37 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'AdvancedTicTacToe';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('X', { static: true }) xImage!: ElementRef<HTMLImageElement>;
+  @ViewChild('O', { static: true }) oImage!: ElementRef<HTMLImageElement>;
+  private isXTurn: boolean = true;
+
+  ngAfterViewInit(): void {
+    this.addEventListeners();
+  }
+
+  private addEventListeners() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+      cell.addEventListener('click', (event) => {
+        this.drawShape(event.target as HTMLButtonElement);
+      });
+    });
+  }
+
+  public drawShape(button: HTMLButtonElement): void {
+    if (button.hasChildNodes()) return;
+
+    const img = this.isXTurn
+      ? this.xImage.nativeElement
+      : this.oImage.nativeElement;
+    const imgElement = img.cloneNode(true) as HTMLImageElement;
+    imgElement.hidden = false;
+    button.appendChild(imgElement);
+
+    button.disabled = true;
+    this.isXTurn = !this.isXTurn;
+  }
 }
